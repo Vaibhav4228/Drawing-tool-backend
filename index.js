03c675b0c327294c65c403cf5939d399d46dc377
Vaibhav422 <92776886+Vaibhav992@users.noreply.github.com>
2024-03-01 01:22:28 +0530

    server and client side is done

--- index.js
+++ index.js
@@ -10,6 +10,19 @@ const io = new Server(httpServer, { cors: 'http://localhost:3000'});
 
 io.on("connection", (socket) => {
   console.log("server connected")
+  
+  //if i am sender so changes done by other clients not me so that's why i am using emit 
+  socket.on('beginPath', (arg)=>{
+     socket.broadcast.emit('beginPath', arg)
+  })
+
+  socket.on('drawLine', (arg) => {
+    socket.broadcast.emit('drawLine', arg)
+  })
+  socket.on('changeConfig', (arg) => {
+    socket.broadcast.emit('changeConfig', arg)
+  })
+
 });
 
 httpServer.listen(8000);
